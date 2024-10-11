44e4ac163c8e37ff14ba8449cb5f91d528b2e1e8
Sujoy Paul <freelancersujoy@gmail.com>
2024-10-11 20:24:58 +0600

    user schema

---
+++ backend/src/models/user.models.js
@@ -0,0 +1,66 @@
+import mongoose, {mongo, Schema} from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+
+const userSchema = new Schema({
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    first_name: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    last_name: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    avatar: {
+        url: {
+            type: String,
+            required: true
+        },
+        public_id: String
+    },
+    bio: {
+        type: String,
+        maxlength: 150
+    },
+    phone: String,
+    password: {
+        type: String,
+        requiredPaths: [true, "Password is true"]
+    },
+    refreshToken: {
+        type: String
+    }
+
+}, {timestamps: true})
+
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.method.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password) 
+}
+
+export const User = mongoose.model("User", userSchema)

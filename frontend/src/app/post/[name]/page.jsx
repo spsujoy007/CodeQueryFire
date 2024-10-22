510c74456b0fa5f0fadc43b4e6d9790d8b593509
Sujoy Paul <freelancersujoy@gmail.com>
2024-10-23 00:31:17 +0600

    SETUP dynamic metadata for post details

--- frontend/src/app/post/[name]/page.jsx
+++ frontend/src/app/post/[name]/page.jsx
@@ -1,5 +1,33 @@
 import PostDetails from '@/components/Posts/PostDetails';
 import React from 'react';
+import { Metadata } from 'next'
+ 
+export async function generateMetadata({ params, searchParams }) {
+    const gettitle = params.name
+    let decodedString = decodeURIComponent(gettitle);
+
+    const res = await fetch('https://mocki.io/v1/4f69807c-4aa7-426f-884d-d51554599b80');
+    const data = await res.json();
+
+    return {
+        title: data?.title,
+        details: data?.details,
+
+        // open graph 
+        openGraph: {
+            title: data?.title,
+            details: data?.details,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/cloudinarybysp/image/upload/v1685516074/y3juhac1c3tp4b1txsiv.png',
+                    width: 800,
+                    height: 500,
+                    alt: data?.title
+                }
+            ]
+        }
+    }
+}
 
 const PostDetailsPage = async({params, searchParams}) => {
     const gettitle = params.name
